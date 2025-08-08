262de07049e6efa459c763e13d81a5e7a00ec830
Tomoyo4396 <463659267@qq.com>
2025-08-08 23:45:05 +0800

    Add files via upload

---
+++ 分可乐.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	float a;
+	int b;
+	scanf("%f %d",&a, &b);
+	float c;
+	c =  a / b;
+	int d;
+	d = 2 * b;
+	printf("%.3f\n%d", a=c, d);
+	return 0;
+}
